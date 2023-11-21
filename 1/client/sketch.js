da532a23251b68bd86fca8e0f78035d7f359ab14
t0a5ted <flameburstunstoppable@gmail.com>
2023-11-21 18:52:01 +0800

    1: base multiplayer moving balls

--- 1/client/sketch.js
+++ 1/client/sketch.js
@@ -1,14 +1,53 @@
 let ball;
 
-const ioClient = io.connect("ws://localhost:8001");
+const socket = io.connect("ws://localhost:8001");
+let em = new EntityManager();
 
 function setup() {
-	new Canvas(500, 500);
+	new Canvas("fullscreen");
 
 	ball = new Sprite();
 	ball.diameter = 50;
 }
 
 function draw() {
-	background('grey');
+    background('grey');
+    move();
+    socket.emit("position", ball.pos.x, ball.pos.y);
 }
+
+function move() {
+    const SPEED = 10;
+    if (kb.pressing("w")) {
+        ball.pos.y -= SPEED;
+    }
+    if (kb.pressing("a")) {
+        ball.pos.x -= SPEED;
+    }
+    if (kb.pressing("s")) {
+        ball.pos.y += SPEED;
+    }
+    if (kb.pressing("d")) {
+        ball.pos.x += SPEED;
+    }
+}
+
+socket.on("playerDataUpdate", (id, playerData) => {
+    for (let data of playerData) {
+        if (data.id === id)
+            continue;
+        if (!em.exists(data.id)) {
+            em.registerNewPlayer(data);
+        } else {
+            em.updatePlayerData(data);
+        }
+    }
+})
+
+socket.on("removeClient", id => {
+    let playerData = entities.get(id);
+    if (playerData) {
+        playerData.sprite.remove();
+        entities.delete(id);
+    }
+});
--- 1/server/server.js
+++ 1/server/server.js
@@ -5,8 +5,43 @@ const io = new Server(8001, {
       origin: "*",
     },
 });
+const clients = new Set();
+const TICK_DELAY = 1000 / 60;
+
+function Client(socket) {
+    this.socket = socket;
+    this.position = {x: 0, y: 0};
+}
+
 console.log("Server running...")
 
 io.on("connection", socket => {
     console.log("New connection!");
+    let client = new Client(socket);
+    clients.add(client);
+
+    socket.on("position", (x, y) => {
+        client.position = {x, y};
+    });
+
+    socket.on("disconnect", () => {
+        clients.delete(client);
+        clients.forEach(c => {
+            c.socket.emit("removeClient", socket.id);
+        })
+    })
 })
+
+function tick() {
+    let allData = [...clients].map(c => {
+        return {
+            position: c.position,
+            id: c.socket.id
+        }
+    });
+    for (let c of clients) {
+        c.socket.emit("playerDataUpdate", c.socket.id, allData);
+    }
+}
+
+setInterval(tick, TICK_DELAY);
