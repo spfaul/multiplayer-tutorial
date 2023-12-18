5e92adab0bb09ee3b8642881fbfb0b5a56ea6b38
t0a5ted <flameburstunstoppable@gmail.com>
2023-12-18 23:09:03 +0800

    2(feat): rooms

--- 2/client/sketch.js
+++ 2/client/sketch.js
@@ -1,8 +1,58 @@
 let ball;
 let em;
 let camManager;
+let currentRoomCode = null;
 
 const socket = io.connect("ws://localhost:8001");
+
+window.onload = () => {
+    const join_option_input = prompt('Select: "CREATE" or "JOIN"', "CREATE");
+    if (join_option_input === "CREATE") {
+        socket.emit("requestCreateRoom");
+    } else if (join_option_input === "JOIN") {
+        const room_code_input = prompt("Enter Room Code");
+        socket.emit("requestJoinRoom", room_code_input);
+    } else {
+        window.onload();
+    }
+};
+
+socket.on("setRoomCode", (code) => {
+    currentRoomCode = code;
+});
+
+socket.on("buildMap", (mapData) => {
+    for (const blockData of Object.values(mapData.blocks)) {
+        let group = new Group();
+        Object.assign(group, blockData);
+        new Tiles(
+            blockData.tileMap,
+            blockData.startX,
+            blockData.startY,
+            blockData.w,
+            blockData.h
+        );
+    }
+});
+
+socket.on("playerDataUpdate", (id, playerData) => {
+    for (let data of playerData) {
+        if (data.id === id) continue;
+        if (!em.exists(data.id)) {
+            em.registerNewPlayer(data);
+        } else {
+            em.updatePlayerData(data);
+        }
+    }
+});
+
+socket.on("removeClient", (id) => {
+    let playerData = em.get(id);
+    if (playerData) {
+        playerData.sprite.remove();
+        em.delete(id);
+    }
+});
 
 function setup() {
     new Canvas("fullscreen");
@@ -10,9 +60,31 @@ function setup() {
     camManager = new CameraManager(camera);
     ball = createPlayerSprite();
     camManager.setTarget(ball);
+    // p5play draws over our draw() loop, so we
+    // have to jump thru hoops to draw our text
+    // over our sprites...... by making a another
+    // sprite. wow.
+    let text_layer = new Sprite();
+    text_layer.visible = false;
+    text_layer.collider = "none";
+    text_layer.update = () => {
+        textAlign(CENTER, CENTER);
+        textSize(32);
+        text(`Room Code: ${currentRoomCode}`, 0, 50, width, 50);
+    };
 }
 
 function draw() {
+    if (!currentRoomCode) {
+        allSprites.visible = false;
+        push();
+        background("white");
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Room Not Found", 0, 0, width, height);
+        pop();
+        return;
+    }
     background("grey");
     move();
     interpolateOtherPlayers();
@@ -68,36 +140,3 @@ function move() {
         ball.pos.x += SPEED;
     }
 }
-
-socket.on("buildMap", (mapData) => {
-    for (const blockData of Object.values(mapData.blocks)) {
-        let group = new Group();
-        Object.assign(group, blockData);
-        new Tiles(
-            blockData.tileMap,
-            blockData.startX,
-            blockData.startY,
-            blockData.w,
-            blockData.h
-        );
-    }
-});
-
-socket.on("playerDataUpdate", (id, playerData) => {
-    for (let data of playerData) {
-        if (data.id === id) continue;
-        if (!em.exists(data.id)) {
-            em.registerNewPlayer(data);
-        } else {
-            em.updatePlayerData(data);
-        }
-    }
-});
-
-socket.on("removeClient", (id) => {
-    let playerData = em.get(id);
-    if (playerData) {
-        playerData.sprite.remove();
-        em.delete(id);
-    }
-});
--- 2/server/server.js
+++ 2/server/server.js
@@ -7,6 +7,7 @@ const io = new Server(8001, {
     },
 });
 const clients = new Set();
+const rooms = [];
 const TICK_DELAY = 1000 / 60;
 const MAPS_DATA = JSON.parse(readFileSync("./maps.json"));
 
@@ -15,19 +16,66 @@ function Client(socket) {
     this.position = { x: 0, y: 0 };
 }
 
+class Room {
+    constructor(id) {
+        this.clients = [];
+        this.id = id;
+    }
+
+    addClient(c) {
+        if (c.room) {
+            throw Error(`Client ${c.socket.id} already in room`);
+        }
+        this.clients.push(c);
+        c.room = this;
+    }
+
+    removeClient(c) {
+        if (!this.clients.includes(c)) {
+            throw Error(`No client ${c.socket.id} in room`);
+        }
+        this.clients.splice(this.clients.indexOf(c), 1);
+        if (this.clients.length === 0) {
+            rooms.splice(rooms.indexOf(this), 1);
+        }
+    }
+}
+
 console.log("Server running...");
 
 io.on("connection", (socket) => {
     console.log("New connection!");
     let client = new Client(socket);
     clients.add(client);
-    socket.emit("buildMap", MAPS_DATA.myWorld);
 
     socket.on("position", (x, y) => {
         client.position = { x, y };
     });
 
+    socket.on("requestCreateRoom", () => {
+        const roomCode = generateRandomRoomCode();
+        let room = new Room(roomCode);
+        room.addClient(client);
+        rooms.push(room);
+        socket.emit("setRoomCode", roomCode);
+        socket.emit("buildMap", MAPS_DATA.myWorld);
+    });
+
+    socket.on("requestJoinRoom", (code) => {
+        for (let room of rooms) {
+            if (room.id === code) {
+                room.addClient(client);
+                socket.emit("setRoomCode", code);
+                socket.emit("buildMap", MAPS_DATA.myWorld);
+                return;
+            }
+        }
+    });
+
     socket.on("disconnect", () => {
+        if (client.room) {
+            client.room.removeClient(client);
+        }
         clients.delete(client);
         clients.forEach((c) => {
             c.socket.emit("removeClient", socket.id);
@@ -35,15 +83,21 @@ io.on("connection", (socket) => {
     });
 });
 
+function generateRandomRoomCode() {
+    return (+new Date()).toString(36).slice(-5);
+}
+
 function tick() {
-    let allData = [...clients].map((c) => {
-        return {
-            position: c.position,
-            id: c.socket.id,
-        };
-    });
-    for (let c of clients) {
-        c.socket.emit("playerDataUpdate", c.socket.id, allData);
+    for (let room of rooms) {
+        let allData = [...room.clients].map((c) => {
+            return {
+                position: c.position,
+                id: c.socket.id,
+            };
+        });
+        for (let c of room.clients) {
+            c.socket.emit("playerDataUpdate", c.socket.id, allData);
+        }
     }
 }
 
